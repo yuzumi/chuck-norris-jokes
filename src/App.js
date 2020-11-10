d8bda7e5b2a0fd7ac10b2276a39618bdded50a57
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-11-10 19:31:27 +0200

    feat: fetch and render jokes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Typography, Container, Card, CardContent, CardActions } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    marginBottom: 20
+  },
+  cardContent: {
+    padding: 24
+  }
+});
 
 const App = () => {
+  const [jokes, setJokes] = useState([]);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetch('https://api.icndb.com/jokes')
+      .then(response => response.json())
+      .then(data => setJokes(data.value))
+      .catch(console.error);
+  }, []);
 
   return (
-    <div className="app"></div>
+    <div className="app">
+      <CssBaseline />
+      <Container>
+        <Typography variant="h2" align="center">
+          Chuck Norris Jokes
+        </Typography>
+
+        {jokes.map(joke => (
+          <Card key={joke.id} className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography>
+                {joke.joke}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Container>
+    </div>
   );
 };
 
