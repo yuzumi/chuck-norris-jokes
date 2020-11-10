3b37b184d46b6e266627e6ce2bb5d2e264f18ff0
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-11-10 20:29:25 +0200

    feat: render liked jokes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { CssBaseline, Typography, Container, Card, CardContent, CardActions, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { 
+  CssBaseline,
+  AppBar,
+  Tabs,
+  Tab,
+  Typography, 
+  Container, 
+  Card, 
+  CardContent, 
+  CardActions, 
+  Button 
+} from '@material-ui/core';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 
 import Category from 'components/Category';
 
@@ -20,6 +31,8 @@ const App = () => {
   const [jokes, setJokes] = useState([]);
   const [likedJokes, setLikedJokes] = useState([]);
   const [visibleJokes, setVisibleJokes] = useState([]);
+
+  const [currentTab, setCurrentTab] = useState(0)
 
   const classes = useStyles();
 
@@ -48,6 +61,10 @@ const App = () => {
     setLikedJokes(newLikedJokes);
   };
 
+  const changeTab = (_event, value) => {
+    setCurrentTab(value);
+  };
+
   return (
     <div className="app">
       <CssBaseline />
@@ -56,38 +73,82 @@ const App = () => {
           Chuck Norris Jokes
         </Typography>
 
-        {visibleJokes.map(joke => (
-          <Card key={joke.id} className={classes.card}>
-            <CardContent className={classes.cardContent}>
-              {joke.categories.length > 0 ? (
-                joke.categories.map(category => (
-                  <Category key={category} label={category} />
-                ))
-              ) : (
-                <Category label="regular" />
-              )}
-              <Typography>
-                {joke.joke}
-              </Typography>
-            </CardContent>
-            <CardActions className={classes.cardActions}>
-              <Button 
-                variant="contained" 
-                color="primary" 
-                onClick={() => likeJoke(joke.id)}
-              >
-                Like
-              </Button>
-              <Button 
-                variant="contained" 
-                color="secondary"
-                onClick={() => unlikeJoke(joke.id)}
-              >
-                Unlike
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
+        <AppBar style={{ marginBottom: 20 }} position="sticky">
+          <Tabs value={currentTab} onChange={changeTab} centered>
+            <Tab id="home-tab" label="Home" aria-controls="home-panel" />
+            <Tab id="like-tab" label="Likes" aria-controls="like-panel" />
+          </Tabs>
+        </AppBar>
+
+        <div role="tabpanel" hidden={currentTab !== 0}>
+          {visibleJokes.map(joke => (
+            <Card key={joke.id} className={classes.card}>
+              <CardContent className={classes.cardContent}>
+                {joke.categories.length > 0 ? (
+                  joke.categories.map(category => (
+                    <Category key={category} label={category} />
+                  ))
+                ) : (
+                  <Category label="regular" />
+                )}
+                <Typography>
+                  {joke.joke}
+                </Typography>
+              </CardContent>
+              <CardActions className={classes.cardActions}>
+                <Button 
+                  variant="contained" 
+                  color="primary" 
+                  onClick={() => likeJoke(joke.id)}
+                >
+                  Like
+                </Button>
+                <Button 
+                  variant="contained" 
+                  color="secondary"
+                  onClick={() => unlikeJoke(joke.id)}
+                >
+                  Unlike
+                </Button>
+              </CardActions>
+            </Card>
+          ))}
+        </div>
+
+        <div role="tabpanel" hidden={currentTab !== 1}>
+          {likedJokes.map(joke => (
+              <Card key={joke.id} className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  {joke.categories.length > 0 ? (
+                    joke.categories.map(category => (
+                      <Category key={category} label={category} />
+                    ))
+                  ) : (
+                    <Category label="regular" />
+                  )}
+                  <Typography>
+                    {joke.joke}
+                  </Typography>
+                </CardContent>
+                <CardActions className={classes.cardActions}>
+                  <Button 
+                    variant="contained" 
+                    color="primary" 
+                    onClick={() => likeJoke(joke.id)}
+                  >
+                    Like
+                  </Button>
+                  <Button 
+                    variant="contained" 
+                    color="secondary"
+                    onClick={() => unlikeJoke(joke.id)}
+                  >
+                    Unlike
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+        </div>
       </Container>
     </div>
   );
